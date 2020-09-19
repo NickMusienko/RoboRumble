5d847d832487adae69f2fd7aa05810fc45303daf
Ian Matthews <ianmatthews@oakland.edu>
2020-09-19 09:30:30 -0400

    Moved Keypad variables to Keypad_Ctrl.h
    Was getting re-declaration errors while working on now_net.cpp so I moved variables for the keypad from definitions to the new header. Also make the selectBot() function to clean things up

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -1,6 +1,4 @@
 
-
-
 uint8_t pos[4][8] = {{0,0,0,0,0,0,0,0},
                      {0,0,0,0,0,0,0,0},
                      {0,0,0,0,0,0,0,0},
@@ -12,7 +10,7 @@ uint8_t bot_tar = 8;
 #include "definitions.h";
 #include "now_net.h";
 
-#include <Adafruit_Keypad.h>;
+#include "Keypad_Ctrl.h"
 #include <Keypad.h>
 
 Keypad customKeypad = Keypad(makeKeymap(hexaNums), rowPins, colPins, ROWS, COLS);
@@ -25,32 +23,14 @@ void setup() {
 void loop() {
   uint8_t customKey = customKeypad.getKey();
 
-  for(uint8_t i=0; i<5;i++){digitalWrite(LED[i],LOW);}
-  if(bot_tar < 4){digitalWrite(LED[bot_tar+1], HIGH);}
-  else{digitalWrite(LED[0],HIGH);}
-
   if(customKey){
-    switch (customKey){
-      case 3:
-        bot_tar = 0;
-        break;
-      case 7:
-        bot_tar = 1;
-        break;
-      case 11:
-        bot_tar = 2;
-        break;
-      case 15:
-        bot_tar = 3;
-        break;
-      case 12:
-        bot_tar = 8;
-      default:
-        break;
-    }
-    Serial.println(customKey);
+    Serial.print(customKey);
+    bot_tar = selectBot(customKey, bot_tar);
+    for(uint8_t i=0; i<5;i++){digitalWrite(LED[i],LOW);}
   }
 
+  if(bot_tar < 4){digitalWrite(LED[bot_tar+1], HIGH);}
+  else{digitalWrite(LED[0],HIGH);}
 
   delay(10);
 }
--- Controller/definitions.h
+++ Controller/definitions.h
@@ -10,25 +10,6 @@
 #define WIFI_CHANNEL 1
 
 
-const uint8_t ROWS = 4;
-const uint8_t COLS = 4;
-uint8_t rowPins[ROWS] = {19, 18, 5, 17};
-uint8_t colPins[COLS] = {16, 4, 0, 2};
 
-char hexaKeys[ROWS][COLS] = {
-  {'1', '2', '3', 'A'},
-  {'4', '5', '6', 'B'},
-  {'7', '8', '9', 'C'},
-  {'*', '0', '#', 'D'}
-};
-
-uint8_t hexaNums[ROWS][COLS] = {
-  {0,1,2,3},
-  {4,5,6,7},
-  {8,9,10,11},
-  {12,13,14,15}
-};
-
-uint8_t LED[5] = {32, 33, 25, 26, 27};
 
 #endif
